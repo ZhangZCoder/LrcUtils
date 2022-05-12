6767b2246d85870a0fccd02f7c25d1d04aaac0a8
zhangzheng <zhangzheng@liquidnetwork.com>
2022-05-12 13:41:38 +0800

    FirstCommit

---
+++ lrclibrary/src/main/java/com/zz/lrclibrary/lrc/utils/LyricsIOUtils.java
@@ -0,0 +1,58 @@
+package com.zz.lrclibrary.lrc.utils;
+
+
+import com.zz.lrclibrary.lrc.formats.LyricsFileReader;
+import com.zz.lrclibrary.lrc.formats.vtt.VttLyricsFileReader;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class LyricsIOUtils {
+    private static ArrayList<LyricsFileReader> readers;
+
+    static {
+        readers = new ArrayList<>();
+        readers.add(new VttLyricsFileReader());
+    }
+
+    /**
+     * 获取支持的歌词文件格式
+     *
+     * @return
+     */
+    public static List<String> getSupportLyricsExts() {
+        List<String> lrcExts = new ArrayList<String>();
+        for (LyricsFileReader lyricsFileReader : readers) {
+            lrcExts.add(lyricsFileReader.getSupportFileExt());
+        }
+        return lrcExts;
+    }
+
+    /**
+     * 获取歌词文件读取器
+     *
+     * @param file
+     * @return
+     */
+    public static LyricsFileReader getLyricsFileReader(File file) {
+        return getLyricsFileReader(file.getName());
+    }
+
+    /**
+     * 获取歌词文件读取器
+     *
+     * @param fileName
+     * @return
+     */
+    public static LyricsFileReader getLyricsFileReader(String fileName) {
+        String ext = Utils.getFileExt(fileName);
+        for (LyricsFileReader lyricsFileReader : readers) {
+            if (lyricsFileReader.isFileSupported(ext)) {
+                return lyricsFileReader;
+            }
+        }
+        return null;
+    }
+}
